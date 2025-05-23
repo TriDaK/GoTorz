dee1b62769eed667c2185c6d635df90334fa9096
TriDaK <trinekremmling@gmail.com>
2025-05-23 10:14:45 +0200

    Updated check in and out time. Migrated and updated DB

--- HotelAPI/HotelAPI/Migrations/20250523081129_UdpatedCheckInOut.Designer.cs
+++ HotelAPI/HotelAPI/Migrations/20250523081129_UdpatedCheckInOut.Designer.cs
@@ -4,6 +4,7 @@ using HotelAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HotelAPI.Migrations
 {
     [DbContext(typeof(HotelDBContext))]
-    partial class HotelDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250523081129_UdpatedCheckInOut")]
+    partial class UdpatedCheckInOut
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
